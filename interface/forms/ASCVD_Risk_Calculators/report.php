84ea3a18c4a3472eb3658c2f49da9f8ddc56d11f
Antonino Daplas <adaplas@gmail.com>
2022-07-31 17:35:11 +0800

    ASCVD Risk: Fix reporting display so it's more readable

--- interface/forms/ASCVD_Risk_Calculators/report.php
+++ interface/forms/ASCVD_Risk_Calculators/report.php
@@ -2,7 +2,26 @@
 //------------report.php
 require_once("../../globals.php");
 require_once($GLOBALS["srcdir"]."/api.inc");
+
 function ASCVD_Risk_Calculators_report( $pid, $encounter, $cols, $id) {
+
+	$replace_array = [
+		"Sbp" => "Systolic BP (mmHg)",
+		"Hdl" => "HDL (mg/dL)",
+		"Bmi" => "BMI (kg/m2)",
+		"Tot Chol" => "Total Cholesterol (mg/dL)",
+		"Bp Med" => "On BP medications",
+		"Lipid Med" => "On Lipid Medications/Statins",
+		"Fh Heartattack" => "Family History of Heart Attacks",
+		"Cac" => "Coronary Artery Calcification (Agatston)",
+		"10y Accaha" => "ACC/AHA 2013 ASCVD risk score (%)",
+		"10y Frs" => "Framingham 2008 ASCVD risk score (with lab measurement) (%)",
+		"10y Frs Simple" => "Framingham 2008 ASCVD risk score (no lab measurement) (%)", 
+		"10y Mesa" => "MESA 2015 CHD risk score (%)",
+		"10y Mesa Cac" => "MESA 2015 CHD risk score with CAC (%)",
+	];
+	
+	
 	$count = 0;
 	$data = formFetch("form_ASCVD_Risk_Calculators", $id);
 	if ($data) {
@@ -18,11 +37,19 @@ function ASCVD_Risk_Calculators_report( $pid, $encounter, $cols, $id) {
 			$key=ucwords(str_replace("_"," ",$key));
 			$mykey = $key;
 			$myval = $value;
-			print "<td><span class=bold>".text($mykey).": </span><span class=text>".text($myval)."</span></td>";
+			if ($mykey == "10y Accaha") {
+				$cols = 1;
+				$count = 0;
+				print "</tr><tr>\n";
+				print "<td><br><hr><span class=bold> Risk Scores</span><br><hr></td>";
+				print "</tr><tr>\n";
+				
+			}
+			print "<td><span class=bold>".text(($replace_array[$mykey]) ? $replace_array[$mykey] : $mykey).": </span><span class=text>".text($myval)."</span></td>";
 			$count++;
 			if ($count == $cols) {
 				$count = 0;
-			print "</tr><tr>\n";
+				print "</tr><tr>\n";
 			}
 		}	
 	}
